2aec179db7fcf9a81209aadad45b235cd3da545c
Suprith-Reddy <suprithgurudu@gmail.com>
2020-10-06 19:51:52 -0400

    SVM & Linear Regression Updates

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from MultiLayerPerceptron import MLPClassification,MLPRegression
 from knn import perform_classification, perform_regression
-
+from SVM import svm_classifier
+from Linear Regression import linear_regressor
 
 
 
@@ -22,7 +23,10 @@ def main():
     classifiers['knn']['final']=perform_classification('datasets-part1/tictac_final.txt')
     classifiers['svm']={}
     print("MLP:")
-
+    classifiers['svm']['single']=svm_classifier('datasets-part1/tictac_single.txt')
+    classifiers['svm']['multi']=svm_classifier('datasets-part1/tictac_multi.txt')
+    classifiers['svm']['final']=svm_classifier('datasets-part1/tictac_final.txt')
+    print("SVM:")
 
 
 if __name__ == "__main__":
